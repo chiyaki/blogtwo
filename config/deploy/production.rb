b00a43dc5b78a18dd58bac1e74d1037da405cdea
chiyaki <66chiyaki@gmail.com>
2016-12-23 04:40:44 +0800

    config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,9 @@ server "139.162.38.232", user: "deploy", roles: %w{app db web}, my_property: :my
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   forward_agent: true,
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
